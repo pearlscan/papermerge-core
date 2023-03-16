275b7fa549eacf5b51fc3e8414aefaf987c9b6a3
Gary Hughes <gary@overthere.co.uk>
2023-03-16 08:40:45 +0000

    Optimised kvstore sanitizing.

--- papermerge/core/views/utils.py
+++ papermerge/core/views/utils.py
@@ -5,28 +5,22 @@ def sanitize_kvstore(kvstore_dict):
     """
     Creates a sanitized dictionary.
 
-    Sanitizied dictionary contains only allowed keys and escaped values.
+    Sanitized dictionary contains only allowed keys and escaped values.
     """
-    allowed_keys = [
+    allowed_keys = {
         'id',
         'key',
         'value',
         'kv_type',
         'kv_format',
         'kv_inherited',
-    ]
+    }
 
-    sanitized_kvstore_dict = {}
-
-    for allowed_key in allowed_keys:
-        if allowed_key in kvstore_dict.keys():
-            value = kvstore_dict.get(allowed_key, None)
-            if isinstance(value, bool):
-                allowed_value = value
-            else:
-                allowed_value = escape(kvstore_dict.get(allowed_key, None))
-
-            sanitized_kvstore_dict[allowed_key] = allowed_value
+    sanitized_kvstore_dict = {
+        k: v if isinstance(v, bool) else escape(v)
+        for k, v in kvstore_dict.items()
+        if k in allowed_keys
+    }
 
     return sanitized_kvstore_dict
 
